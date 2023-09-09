5a0eb4e688a106719e0eecd5b945a76f6d2639c8
Gianfranco Caschetto <papancho7@gmail.com>
2023-09-08 22:18:22 -0400

    diana agregada y documentacion

--- Simulador_Dardos/src/App.jsx
+++ Simulador_Dardos/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Diana } from "./hooks/diana.mjs";
 import { Coordenadas } from "./hooks/coordenadas.mjs";
 import { useCallback } from "react";
@@ -6,7 +6,7 @@ import { useCallback } from "react";
 const diana = new Diana();
 let puntuaciones = {};
 
-//Inicializa las puntuaciones con 0
+// Inicializa las puntuaciones con 0
 for (let i = 0; i <= 60; i++) {
   puntuaciones = { ...puntuaciones, [i]: 0 };
 }
@@ -14,17 +14,41 @@ for (let i = 0; i <= 60; i++) {
 function App() {
   const [notificacion, setNotificacion] = useState({ message: "" });
   const [puntajeActual, setPuntajeActual] = useState(null);
-  //Hacerlo con un boton para que cada vez que lance me diga la probabilidad y llevar un contador para asi pasar a la siguiente puntuacion
+  const [mostrarDardo, setMostrarDardo] = useState(false); // Estado para mostrar u ocultar el círculo del dardo
+  const [coordenadas, setCoordenadas] = useState(new Coordenadas()); // Estado para almacenar las coordenadas del mouse
   const actualizarPuntos = useCallback(() => {
     const coord = new Coordenadas();
     const puntos = diana.getPuntos(coord);
     puntuaciones = { ...puntuaciones, [puntos]: puntuaciones[puntos] + 1 };
     setNotificacion({ message: `Has hecho: ${puntos} puntos` });
     setPuntajeActual(puntos);
+    setCoordenadas(coord);
+    // Mostrar el círculo del dardo durante un breve período de tiempo
+    setMostrarDardo(true);
+    setTimeout(() => {
+      setMostrarDardo(false);
+    }, 4000); // Cambia esto según la duración que desees para mostrar el círculo del dardo
   }, []);
 
   return (
     <div className="text-center m-8">
+      <div style={{ position: "relative" }}>
+        <img
+          src="https://dbdzm869oupei.cloudfront.net/img/sticker/preview/4572.png"
+          alt=""
+          className="mx-auto mb-8"
+        />
+        {mostrarDardo && (
+          <div
+            className="w-4 h-4 bg-blue-500 rounded-full border border-white"
+            style={{
+              position: "absolute",
+              top: `${50 - coordenadas.getY()}%`,
+              left: `${50 - coordenadas.getX()}%`,
+            }}
+          ></div>
+        )}
+      </div>
       <button
         onClick={actualizarPuntos}
         className="bg-gray-900 text-white px-8 py-2 rounded"
